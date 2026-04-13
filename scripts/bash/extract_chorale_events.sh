32de9456c570b16468a2e6ed4db859353507a7ae
ManMls <mollismanuel07@gmail.com>
2026-04-13 17:56:01 +0200

    fix

--- scripts/bash/extract_chorale_events.sh
+++ scripts/bash/extract_chorale_events.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+#!/bin/env bash
 
 
-DATASET="../data/raw/bach_choral_set_dataset.xls"
-OUTDIR="../data/processed"
+DATASET="../../data/raw/bach_choral_set_dataset.xls"
+OUTDIR="../../data/processed/"
 
 
 csvcut -c 1,3-14 "$DATASET" | tail -n +2 | sed -e 's/YES/1/g' -e 's/NO/0/g' | awk -F',' -v out="$OUTDIR" '
 {
-    file = out "/" $1
+    file = out $1
     $1 = ""
     sub(/^,/, "")
-    print >> (file ".xls")
+    print >> (file)
 }'
